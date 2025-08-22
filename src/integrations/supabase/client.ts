81c5cf844b8ccf716fc8f30f82739c8b41868cf9
Zack Lau <zhan.you.lau@sap.com>
2025-08-23 02:31:20 +0800

    fix: changed public anon API key to use env variable

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://hpxoifxeswbiqwzzhglc.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhweG9pZnhlc3diaXF3enpoZ2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3ODk0MjMsImV4cCI6MjA3MTM2NTQyM30.EKcV7sZO9S8_UZHCI2i14Nb9ixl33-axZxs0MRwNUF0";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
